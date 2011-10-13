9c39a9ce6f76c25ef93a26d302c497d41a7518fe
Christopher Weber <seed@seed-think.bfh.ch>
2011-10-13 13:15:57 +0200

    some enhancments ;)

--- VR_exercise-01-minarett/src/house.cpp
+++ VR_exercise-01-minarett/src/house.cpp
@@ -10,8 +10,8 @@ using namespace std;
 
 #include "zpr.h"
 
-const float MINARETT_DIAMETER = 2.0F;     // width of the minarett
-const float MINARETT_HEIGHT = 16.0F;    // height of the minarett
+const float HEIGHT = 12.0F;
+const float WIDTH = 10.0F;
 
 
 void checkErrorCode(void)
@@ -29,83 +29,98 @@ void checkErrorCode(void)
 
 
 
-void drawHouse(float height, float diameter)
+void drawHouse(float height, float width)
 /////////////////////////////////////////////////////////////////////////////////////////
 {
 	// draw ground
 
+	float baseRight = (width/2);
+	float baseLeft = 0.0f - (width/2);
+	float baseFront = baseRight;
+	float baseBack = baseLeft;
+	float baseHeight = 0.0f - (height/2);
 
-	float base = 0.0 - (height/2);
-	float towerHeight = (height/2);
 
 
-	//Fundament
+	//building ground
+
 	glColor3f(0.0, 0.75, 0.0);
-	//base
-	glEnable(GL_NORMALIZE);
-
     glBegin(GL_QUADS);
-
-    glVertex3f(diameter,base, diameter);
-		glVertex3f(diameter,  base, 0.0f);
-		glVertex3f(0.0f, base, 0.0f);
-		glVertex3f(0.0f, base, diameter);
-		glVertex3f(diameter,base, diameter);
+    	glVertex3f(baseLeft,baseHeight,baseBack);
+    	glVertex3f(baseLeft,baseHeight,baseFront);
+    	glVertex3f(baseRight,baseHeight,baseFront);
+    	glVertex3f(baseRight,baseHeight,baseBack);
     glEnd();
 
-    float sectionHeight = towerHeight - ((height/13) * 2);
+    float wallHeight = baseHeight + (height/16.0);
 
-	//first section
-    glColor3f(1.0F, 0.0F, 0.0F);
-    glBegin(GL_QUADS);
+    glColor3f(0.75,0.0,0.0);
+    glBegin(GL_QUAD_STRIP);
+    	glVertex3f(baseLeft,baseHeight,baseFront);
+    	glVertex3f(baseLeft,wallHeight,baseFront);
+    	glVertex3f(baseLeft,baseHeight,baseBack);
+    	glVertex3f(baseLeft,wallHeight,baseBack);
 
-	glVertex3f(0.0F, base, 0.0F);
-	glVertex3f(diameter,  base, 0.0F);
-	glVertex3f(diameter,sectionHeight, 0.0F);
-	glVertex3f(0.0F,  sectionHeight, 0.0F);
+    	glVertex3f(baseRight,baseHeight,baseBack);
+    	glVertex3f(baseRight,wallHeight,baseBack);
 
-	glVertex3f(0.0F, base, 0.0F);
-	glVertex3f(0.0F,  base, diameter);
-	glVertex3f(0.0F,sectionHeight, diameter);
-	glVertex3f(0.0F,  sectionHeight, 0.0F);
+    	glVertex3f(baseRight,baseHeight,baseFront);
+    	glVertex3f(baseRight,wallHeight,baseFront);
 
-	glVertex3f(0.0F, base, diameter);
-	glVertex3f(diameter,  base, diameter);
-	glVertex3f(diameter,sectionHeight, diameter);
-	glVertex3f(0.0F,  sectionHeight, diameter);
-
-	glVertex3f(diameter, base, diameter);
-	glVertex3f(diameter,  base, 0.0F);
-	glVertex3f(diameter,sectionHeight, 0.0F);
-	glVertex3f(diameter,  sectionHeight, diameter);
-
+    	glVertex3f(baseLeft,baseHeight,baseFront);
+    	glVertex3f(baseLeft,wallHeight,baseFront);
     glEnd();
 
 
-    //dach
+    //the house
+    float houseFront = baseFront - (width / 14);
+    float houseBack = baseBack + (width / 14);
+    float houseLeft = baseLeft + (width / 14);
+    float houseRight = baseRight - (width / 2);
+    float houseHeight = baseHeight + (height / 4);
 
-    glColor3f(0.0F, 0.0F, 1.0F);
-    glBegin(GL_TRIANGLES);
+    glColor3f(0.0,0.0,0.75);
+    glBegin(GL_QUAD_STRIP);
+    	glVertex3f(houseLeft,baseHeight,houseFront);
+    	glVertex3f(houseLeft,houseHeight,houseFront);
+    	glVertex3f(houseLeft,baseHeight,houseBack);
+    	glVertex3f(houseLeft,houseHeight,houseBack);
 
-    glVertex3f((diameter/2),towerHeight,(diameter/2));
-    glVertex3f(0.0F,sectionHeight,0.0F);
-    glVertex3f(0.0F,sectionHeight,diameter);
+    	glVertex3f(houseRight,baseHeight,houseBack);
+    	glVertex3f(houseRight,houseHeight,houseBack);
 
-    glVertex3f((diameter/2),towerHeight,(diameter/2));
-    glVertex3f(0.0F,sectionHeight,diameter);
-    glVertex3f(diameter,sectionHeight,diameter);
+    	glVertex3f(houseRight,baseHeight,houseFront);
+    	glVertex3f(houseRight,houseHeight,houseFront);
 
-    glVertex3f((diameter/2),towerHeight,(diameter/2));
-    glVertex3f(diameter,sectionHeight,diameter);
-    glVertex3f(diameter,sectionHeight,0.0F);
-
-    glVertex3f((diameter/2),towerHeight,(diameter/2));
-    glVertex3f(diameter,sectionHeight,0.0F);
-    glVertex3f(0.0F,sectionHeight,0.0F);
-
+    	glVertex3f(houseLeft,baseHeight,houseFront);
+    	glVertex3f(houseLeft,houseHeight,houseFront);
     glEnd();
 
-    //balkon erster stock ;)
+    float houseRoofFront = houseFront + (width/30);
+    float houseRoofBack = houseBack - (width / 30);
+    float houseRoofLeft = houseLeft - (width / 26);
+    float houseRoofRight = houseRight + (width / 26);
+    float houseRoofHeight = houseHeight + ((houseHeight - baseHeight)/2);
+    float houseRoofMiddle = (houseRoofLeft + houseRoofRight)/2;
+
+    glColor3f(0.75f,0.75f,0.75f);
+    glBegin(GL_TRIANGLE_STRIP);
+    	glVertex3f(houseRoofLeft,houseHeight,houseRoofFront);
+    	glVertex3f(houseRoofRight,houseHeight,houseRoofFront);
+    	glVertex3f(houseRoofMiddle,houseRoofHeight,houseRoofFront);
+
+    	glVertex3f(houseRoofRight,houseHeight,houseRoofBack);
+
+    	glVertex3f(houseRoofMiddle,houseRoofHeight,houseRoofBack);
+
+    	glVertex3f(houseLeft,houseHeight,houseRoofBack);
+
+    	glVertex3f(houseRoofMiddle,houseRoofHeight,houseRoofFront);
+
+    	glVertex3f(houseRoofLeft,houseHeight,houseRoofFront);
+    glEnd();
+
+
 
 
 }
@@ -119,7 +134,7 @@ void display(void)
 
     glClear(GL_COLOR_BUFFER_BIT);
 
-	drawHouse(MINARETT_HEIGHT,MINARETT_DIAMETER);
+	drawHouse(HEIGHT,WIDTH);
 	glFlush();
 
 	glPopMatrix();
